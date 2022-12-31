244884ff6441467242c01a23221f9b83695d80d5
WaqarAliBhatti <WaqarAliBhatti>
2022-12-31 17:35:25 +0500

    Understanding call Apply and Bind Methods

---
+++ bind.js
@@ -0,0 +1,22 @@
+function details(){
+    console.log(`${this.id} earns ${this.salary}`)
+}
+
+const waqar = {
+    id:'waqar',
+    salary:15000,
+    detail:details,
+    about:function(hobby,caste){
+        console.log(`${this.id} is Node JS Dev Hobby is ${hobby} and He's ${caste}`)
+    }
+
+}
+const zorain ={
+    id:'zorain',
+    salary:30000,
+    detail:details
+
+}
+waqar.detail();
+const zora=waqar.about.bind(zorain,["coding","Mughal"]); //if function Called has parameters, we will simply pass arguements to it 
+zora();
---
+++ call.js
@@ -0,0 +1,21 @@
+function details(){
+    console.log(`${this.id} earns ${this.salary}`)
+}
+
+const waqar = {
+    id:'waqar',
+    salary:15000,
+    detail:details,
+    about:function(hobby,caste){
+        console.log(`${this.id} is Node JS Dev Hobby is ${hobby} and He's ${caste}`)
+    }
+
+}
+const zorain ={
+    id:'zorain',
+    salary:30000,
+    detail:details
+
+}
+waqar.detail();
+waqar.about.call(zorain,"coding","Mughal"); //if function Called has parameters, we will simply pass arguements to it 
